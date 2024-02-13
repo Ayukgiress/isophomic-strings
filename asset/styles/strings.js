d782861f96f2bd92d15a7ba952f681972e8c7b27
AyukGiress <giress865@gmail.com>
2024-02-13 16:24:03 +0100

    my checke

--- asset/styles/strings.js
+++ asset/styles/strings.js
@@ -1,35 +1,34 @@
 function isomorphicStrings(str1, str2) {
   if (str1.length !== str2.length) {
-    return "the string entered is not isomorphic.";
+    return 'the string entered is not isomorphic.'
   }
 
-  let map = new Map();
-  let set = new Set();
+  let map = new Map()
+  let set = new Set()
 
   for (let i = 0; i < str1.length; i++) {
-    let char1 = str1.charAt(i);
-    let char2 = str2.charAt(i);
+    let char1 = str1.charAt(i)
+    let char2 = str2.charAt(i)
 
     if (map.has(char1)) {
       if (map.get(char1) !== char2) {
-        return "the string entered is not isomorphic.";
+        return 'the string entered is not isomorphic.'
       }
     } else {
       if (set.has(char2)) {
-        return "the string entered is not isomorphic.";
+        return 'the string entered is not isomorphic.'
       }
-      map.set(char1, char2);
-      set.add(char2);
+      map.set(char1, char2)
+      set.add(char2)
     }
   }
 
-  return "the string entered is isomorphic.";
+  return 'the string entered is isomorphic.'
 }
 
-function checkIsomorphic() {
-  let input1 = document.getElementById("string1").value;
-  let input2 = document.getElementById("string2").value;
-  let result = isomorphicStrings(input1, input2);
-  document.getElementById("result").innerText = "" + result;
+function checkIsomorphic () {
+  let input1 = document.getElementById('string1').value
+  let input2 = document.getElementById('string2').value
+  let result = isomorphicStrings(input1, input2)
+  document.getElementById('result').innerText = "" + result
 }
-
